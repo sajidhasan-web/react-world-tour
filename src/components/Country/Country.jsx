b9dc350918789961135479297770c737b2741ea2
sajidhasan-web <sajidhasan.web@gmail.com>
2024-03-10 22:30:41 +0600

    load countries data and showing some initial data

---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,18 @@
+import './Country.css'
+
+const Country = ({country}) => {
+    console.log(country)
+    
+    const {name, flags, population, area} = country
+
+    return (
+        <div className='countryBox'>
+            <h3>Name: {name?.common}</h3>
+            <img src={flags.png} alt="" />
+            <p>Population: {population}</p>
+            <p>Area: {area}</p>
+        </div>
+    );
+};
+
+export default Country;
